aec9562e949ed4810a10eacc2248a200ccc568f1
Elan Fraiman <elan@elfraiman.com>
2018-06-03 12:28:51 +0200

    Starting to add functions and more design, fixed smaller screen sizes

--- src/app/header/header-content/header-content.component.ts
+++ src/app/header/header-content/header-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-header-content',
@@ -7,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderContentComponent implements OnInit {
   nerdsComeTo = 'Share';
+  nerdsComeToList = ['Develop', 'Improve', 'Excel', 'Learn', 'Meet', 'Hire', 'Play', 'Teach'];
+  nerdsPictureList = ['../../../assets/wolverien.png', '../../../assets/cuteninja.png', '../../../assets/minecraft.png'];
+  nerdPicture = '../../../assets/wolverien.png';
+  inputedEmail = '';
 
-  constructor() { }
+  newContentWord() {
+      const index = Math.floor(Math.random() * this.nerdsComeToList.length);
+      this.nerdsComeTo  = this.nerdsComeToList[index];
+  }
+
+  newNerdPicture() {
+    const index = Math.floor(Math.random() * this.nerdsPictureList.length);
+    this.nerdPicture = this.nerdsPictureList[index];
+  }
+  constructor() {
+    setInterval(() => {
+      this.newContentWord();
+      this.newNerdPicture();
+    }, 3000);
+  }
 
   ngOnInit() {
   }
+
+  emailChange(event) {
+    this.inputedEmail = event.target.value;
+  }
+
+  getStarted() {
+    alert('Thank you for Subscribing! More details have been sent to ' + this.inputedEmail);
+  }
+
+
 
 }
