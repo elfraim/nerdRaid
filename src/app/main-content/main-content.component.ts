de7351bfcf2b7c2f493013cd7e04614500bd765a
Elan Fraiman <elan@elfraiman.com>
2018-06-03 14:41:51 +0200

    added main-content functions and styling, added pictures and more

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent implements OnInit {
+  webDevs = 254;
+  webDesigners = 165;
+  webTeachers = 24;
+  marketingGurus = 67;
+  showTeachersDiv = false;
+  showWebDevDiv = false;
+  showDesignersDiv = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  showTeachersText() {
+    this.showTeachersDiv = true;
+    this.showWebDevDiv = false;
+    this.showDesignersDiv = false;
+  }
+
+  showWebDevText() {
+    this.showWebDevDiv = true;
+    this.showTeachersDiv = false;
+    this.showDesignersDiv = false;
+  }
+
+  showDesignersText() {
+    this.showDesignersDiv = true;
+    this.showWebDevDiv = false;
+    this.showTeachersDiv = false;
+  }
+
+  getStarted() {
+    alert('Thank you for the intrest! This is still under construction');
+  }
 }
